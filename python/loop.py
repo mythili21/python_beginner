66387f3cd9e82cb4b12928aff656de609ed935a8
mythili21 <32951350+mythili21@users.noreply.github.com>
2017-10-21 09:37:01 +0300

    Add files via upload

---
+++ python/loop.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece015
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece015 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+count = 0
+while (count < 9):
+   print 'The count is:', count
+   count = count + 1
+print "Good bye!"
+if __name__ == '__main__':
+    main()
