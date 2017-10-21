9b92b1ace34d71a35ad0b69be8e25196e59768f4
mythili21 <32951350+mythili21@users.noreply.github.com>
2017-10-21 08:41:24 +0300

    Add files via upload

---
+++ python/constants.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece015
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece015 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=18
+if(a<18):
+    print('a is not eligible')
+else:
+        print('a is eligible')
