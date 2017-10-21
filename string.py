55f4fbfabdfee422785e6bbeedaf474d15e871b6
mythili21 <32951350+mythili21@users.noreply.github.com>
2017-10-21 08:07:30 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece015
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece015 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
+mystring='hi from guvi' +'test'
+print(mystring[1:3])
+print(mystring[:2])
+print(mystring[-1])
+print(mystring.split(" "))
